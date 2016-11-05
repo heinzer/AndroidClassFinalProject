0e0a19b477a339b59fb971cfd89e9dc95f44c3cc
AD\heinzer <heinzer@msoe.edu>
2016-11-04 19:26:00 -0500

    setup main menu activity & places list activity

--- FinalProject/app/src/main/java/com/example/heinzer/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/heinzer/finalproject/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.heinzer.finalproject;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startGameButton = (Button) findViewById(R.id.startgame);
+        startGameButton.getBackground().setColorFilter(0xFF5db0ba, PorterDuff.Mode.MULTIPLY);
+/*        final Intent startGameIntent = new Intent(this, NewTaskActivity.class);
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(startGameIntent);
+            }
+        });*/
+
+        Button placesButton = (Button) findViewById(R.id.placeslist);
+        placesButton.getBackground().setColorFilter(0xFF5db0ba, PorterDuff.Mode.MULTIPLY);
+        final Intent placesIntent = new Intent(this, PlacesListActivity.class);
+        placesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(placesIntent);
+            }
+        });
+
     }
 }
--- FinalProject/app/src/main/java/com/example/heinzer/finalproject/PlacesListActivity.java
+++ FinalProject/app/src/main/java/com/example/heinzer/finalproject/PlacesListActivity.java
@@ -2,11 +2,13 @@ package com.example.heinzer.finalproject;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -25,13 +27,23 @@ public class PlacesListActivity extends ListActivity {
             Toast.makeText(this, this.getIntent().getExtras().getString("value"), Toast.LENGTH_SHORT).show();
         }
 
+        Button mainMenuButton = (Button) findViewById(R.id.mainmenu);
+        mainMenuButton.getBackground().setColorFilter(0xFFcc4b69, PorterDuff.Mode.MULTIPLY);
+        final Intent mainMenuIntent = new Intent(this, MainActivity.class);
+        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(mainMenuIntent);
+            }
+        });
+
         //Get a list of places
         ListView listView = getListView();
         //set onclick for each list item
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent placeDetailsIntent = new Intent(getBaseContext(), PlaceDetailsActivity.class);
-                placeDetailsIntent.putExtra("", placeList.get(position));
+                placeDetailsIntent.putExtra("place", placeList.get(position));
                 //redirect to new activity with info/ id
                 startActivity(placeDetailsIntent);
             }
